7069cad7a75d2c1648981cf07dbc1881d9b3c72e
Dylan Barrell <dylan@barrell.com>
2018-03-24 12:46:53 -0700

    feat: Validate the color inputs (#5)
    * refactor: get rid of unused callback
    
    * feat: validate color inputs
    
    * fix: delete temporary element after use

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import { Workspace, Button } from 'cauldron-react';
 import ColorPalette from './color-palette'
 import SelectedColors from './SelectedColors/';
+import rgbHex from 'rgb-hex';
 import './App.css';
+
+function normalizeColor(inputValue) {
+  if (!inputValue || !inputValue.length) {
+    throw new Error('please provide a color');
+  }
+  const span = document.createElement('span');
+  document.body.appendChild(span);
+  span.style.backgroundColor = inputValue;
+  const styl = window.getComputedStyle(span);
+  const bgcolor = styl.backgroundColor;
+  if (!span.style.backgroundColor) {
+    document.body.removeChild(span);
+    throw new Error('please provide a valid color string')
+  }
+  document.body.removeChild(span);
+  return rgbHex(bgcolor);
+}
 
 class App extends Component {
   constructor() {
@@ -33,7 +51,9 @@ class App extends Component {
                 id='add-color'
                 className='dqpl-text-input'
                 ref={el => this.input = el}
+                aria-describedby='error'
               />
+              <div id='error' className='dqpl-error-wrap'>{ this.state.error }</div>
             </div>
             <Button type='submit'>Add color</Button>
           </form>
@@ -48,11 +68,22 @@ class App extends Component {
     e.preventDefault();
     const { colorArray } = this.state;
     const updatedColorArray = colorArray.slice();
+    try {
+      const color = normalizeColor(this.input.value);
+      if (updatedColorArray.includes(color)) {
+        throw new Error('duplicate color');
+      }
+      updatedColorArray.push(color);
 
-    updatedColorArray.push(this.input.value);
-    this.setState({ colorArray: updatedColorArray });
-    localStorage.setItem('colorArray', JSON.stringify(updatedColorArray));
-    this.input.value = '';
+      this.setState({
+        colorArray: updatedColorArray,
+        error: undefined
+      });
+      localStorage.setItem('colorArray', JSON.stringify(updatedColorArray));
+      this.input.value = '';
+    } catch (error) {
+      this.setState({ error: `Incorrect input: ${error.message}` });
+    }
   }
 
   removeColor(index) {
