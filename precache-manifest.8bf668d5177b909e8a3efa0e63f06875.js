a0469ad210ceb635a632509b39d73920c5e897ee
Harris Schneiderman <harris.schneiderman@deque.com>
2019-03-23 22:01:22 -0700

    Updates

---
+++ precache-manifest.8bf668d5177b909e8a3efa0e63f06875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d625f310382dd715922",
+    "url": "/color-palette/static/js/runtime~main.6d625f31.js"
+  },
+  {
+    "revision": "2cff7f427ae2dfd11b69",
+    "url": "/color-palette/static/js/main.2cff7f42.chunk.js"
+  },
+  {
+    "revision": "fd787ba15a1a604228b3",
+    "url": "/color-palette/static/js/1.fd787ba1.chunk.js"
+  },
+  {
+    "revision": "2cff7f427ae2dfd11b69",
+    "url": "/color-palette/static/css/main.c8d2ec58.chunk.css"
+  },
+  {
+    "revision": "608096518e7493a3ba44c65ed8f69148",
+    "url": "/color-palette/index.html"
+  }
+];
